6ed4526471bf32738bb411e4337498f83a27537e
Daniel Adigun <daniel.adigun@digitalforte.ng>
2016-02-10 13:44:58 +0100

    Removed AppHost DB connection for now

--- splice.core/AppHost.cs
+++ splice.core/AppHost.cs
@@ -14,59 +14,60 @@ namespace splice.core
 {
     public class AppHost : AppHostBase
     {
-        public AppHost():base("Splice.core", typeof(SpliceServices).Assembly) {   }
+        public AppHost() : base("Splice.core", typeof(SpliceServices).Assembly) { }
         public override void Configure(Container container)
         {
             SetConfig(new EndpointHostConfig { ServiceStackHandlerFactoryPath = "api.ashx" });
 
-            //------Configure Authentication API-------//
-            var appSettings = new AppSettings();
+            //    //------Configure Authentication API-------//
+            //    var appSettings = new AppSettings();
 
-            //Default route: /auth/{provider}
-            Plugins.Add(new AuthFeature(() => new AuthUserSession(),
-                new IAuthProvider[] {
-                    new CredentialsAuthProvider(appSettings), 
-                    new FacebookAuthProvider(appSettings), 
-                    new TwitterAuthProvider(appSettings), 
-                    new BasicAuthProvider(appSettings),                    
-                               
-                }));
+            //    //Default route: /auth/{provider}
+            //    Plugins.Add(new AuthFeature(() => new AuthUserSession(),
+            //        new IAuthProvider[] {
+            //            new CredentialsAuthProvider(appSettings), 
+            //            new FacebookAuthProvider(appSettings), 
+            //            new TwitterAuthProvider(appSettings), 
+            //            new BasicAuthProvider(appSettings),                    
 
-            //Default route: /register
-            Plugins.Add(new RegistrationFeature());
+            //        }));
 
-              //Requires ConnectionString configured in Web.Config
-            var connectionString = ConfigurationManager.ConnectionStrings["DatabaseContext"].ConnectionString;
-            //var connectionString = DataSource.sqlConnectionString;
+            //    //Default route: /register
+            //    Plugins.Add(new RegistrationFeature());
 
-            //---NOTE: Install ServiceStack.OrmLite.SqlServer for this to always work.
-            container.Register<IDbConnectionFactory>(c =>
-               new OrmLiteConnectionFactory(connectionString, SqlServerDialect.Provider));
+            //      //Requires ConnectionString configured in Web.Config
+            //    var connectionString = ConfigurationManager.ConnectionStrings["DatabaseContext"].ConnectionString;
+            //    //var connectionString = DataSource.sqlConnectionString;
 
-            container.Register<IUserAuthRepository>(c =>
-                new OrmLiteAuthRepository(c.Resolve<IDbConnectionFactory>()));
+            //    //---NOTE: Install ServiceStack.OrmLite.SqlServer for this to always work.
+            //    container.Register<IDbConnectionFactory>(c =>
+            //       new OrmLiteConnectionFactory(connectionString, SqlServerDialect.Provider));
 
-            var authRepo = (OrmLiteAuthRepository)container.Resolve<IUserAuthRepository>();
+            //    container.Register<IUserAuthRepository>(c =>
+            //        new OrmLiteAuthRepository(c.Resolve<IDbConnectionFactory>()));
 
-           
-            //Run this Only once
-            //authRepo.DropAndReCreateTables();
-            authRepo.CreateMissingTables();
+            //    var authRepo = (OrmLiteAuthRepository)container.Resolve<IUserAuthRepository>();
 
-            if (authRepo.GetUserAuthByUserName("daniel.adigun@digitalforte.ng") == null)
-            {
-                var userAuth = new UserAuth
-                {
-                    UserName = null,
-                    Email = "daniel.adigun@digitalforte.ng",
-                    FirstName = "Administrator",
-                    LastName = " ",
-                    RefId = new Random().Next(),
-                    Roles = new List<string> { "Admin" }
-                };
-                authRepo.CreateUserAuth(userAuth, "pass");
-            }
+
+            //    //Run this Only once
+            //    //authRepo.DropAndReCreateTables();
+            //    authRepo.CreateMissingTables();
+
+            //    if (authRepo.GetUserAuthByUserName("daniel.adigun@digitalforte.ng") == null)
+            //    {
+            //        var userAuth = new UserAuth
+            //        {
+            //            UserName = null,
+            //            Email = "daniel.adigun@digitalforte.ng",
+            //            FirstName = "Administrator",
+            //            LastName = " ",
+            //            RefId = new Random().Next(),
+            //            Roles = new List<string> { "Admin" }
+            //        };
+            //        authRepo.CreateUserAuth(userAuth, "pass");
+            //    }
+            
+
         }
-        
     }
 }
